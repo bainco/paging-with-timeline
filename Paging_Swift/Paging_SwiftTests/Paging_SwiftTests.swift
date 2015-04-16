396830b981999aa6a2ba9c59ba98524b48af689b
bainco <bainco@email.sc.edu>
2015-04-15 22:28:46 -0400

    first commit

---
+++ Paging_Swift/Paging_SwiftTests/Paging_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Paging_SwiftTests.swift
+//  Paging_SwiftTests
+//
+//  Created by Olga Dalton on 26/10/14.
+//  Copyright (c) 2014 swiftiostutorials.com. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Paging_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
